4157f5f806b5dd2e38532393b0329aa98ecd75d2
Alejandro Arias <112278828+AariazP@users.noreply.github.com>
2023-04-03 09:31:32 -0500

    Update BinaryTree.java

--- src/main/java/org/alejandroArias/model/BinaryTree.java
+++ src/main/java/org/alejandroArias/model/BinaryTree.java
@@ -1,158 +1,366 @@
 package org.alejandroArias.model;
 
+import java.util.Iterator;
+import java.util.Objects;
+
 /**
  * This class represents a binary tree
  * @author Alejandro
  * @version 1.0.0
  * @since 1.0.0
  */
-public class BinaryTree<T> {
+public class BinaryTree<T  extends Comparable<T>> {
 
+    private int nodeCount = 0;
 
-    private Node<T> root;
-    private int size;
+    // This BST is a rooted tree, so we maintain a handle on the root node
+    private Node root = null;
 
-    public BinaryTree() {
-        this.root = null;
-        this.size = 0;
-    }
+    // Internal node containing node references
+    // and the actual node data
+    private class Node {
+        T data;
+        Node left, right;
 
-    public Node<T> getRoot() {
-        return root;
-    }
-
-    public void setRoot(Node<T> root) {
-        this.root = root;
-    }
-
-    public int getSize() {
-        return size;
-    }
-
-    public void setSize(int size) {
-        this.size = size;
-    }
-
-    /**
-     * This method adds a node to the tree
-     * @param data the data of the node
-     */
-    public void add(T data){
-        Node<T> node = new Node<>(data);
-        if(root == null){
-            root = node;
-        }else{
-            add(root, node);
-        }
-        size++;
-    }
-
-    /**
-     * This method adds a node to the tree
-     * @param root the root of the tree
-     * @param node the node to add
-     */
-    private void add(Node<T> root, Node<T> node){
-        if((int)node.getData() < (int)root.getData()){
-            if(root.getLeft() == null){
-                root.setLeft(node);
-            }else{
-                add(root.getLeft(), node);
-            }
-        }else{
-            if(root.getRight() == null){
-                root.setRight(node);
-            }else{
-                add(root.getRight(), node);
-            }
-        }
-    }
-
-    /**
-     * This method searches a node in the tree
-     * @param data the data of the node
-     * @return true if the node is found, false otherwise
-     */
-    public boolean search(T data){
-        return search(root, data);
-    }
-
-    /**
-     * This method searches a node in the tree
-     * @param root the root of the tree
-     * @param data the data of the node
-     * @return true if the node is found, false otherwise
-     */
-    private boolean search(Node<T> root, T data){
-        if(root == null){
-            return false;
-        }else if(root.getData().equals(data)){
-            return true;
-        }else if((int)data < (int)root.getData()){
-            return search(root.getLeft(), data);
-        }else{
-            return search(root.getRight(), data);
-        }
-    }
-
-    /**
-     * This method prints the tree in pre order
-     */
-    public void printPreOrder(){
-        printPreOrder(root);
-    }
-
-    /**
-     * This method prints the tree in preOrder
-     * @param root the root of the tree
-     */
-
-    private void printPreOrder(Node<T> root) {
-        if (root != null) {
-            System.out.println(root.getData());
-            printPreOrder(root.getLeft());
-            printPreOrder(root.getRight());
-        }
-
-    }
-
-
-
-    private static class Node<T> {
-
-        private T data;
-        private Node<T> left;
-        private Node<T> right;
-
-        public Node(T data) {
-            this.data = data;
-            this.left = null;
-            this.right = null;
-        }
-
-        public T getData() {
-            return data;
-        }
-
-        public void setData(T data) {
-            this.data = data;
-        }
-
-        public Node<T> getLeft() {
-            return left;
-        }
-
-        public void setLeft(Node<T> left) {
+        public Node(Node left, Node right, T elem) {
+            this.data = elem;
             this.left = left;
-        }
-
-        public Node<T> getRight() {
-            return right;
-        }
-
-        public void setRight(Node<T> right) {
             this.right = right;
         }
+    }
 
+    // Check if this binary tree is empty
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    // Get the number of nodes in this binary tree
+    public int size() {
+        return nodeCount;
+    }
+
+    // Add an element to this binary tree. Returns true
+    // if we successfully perform an insertion
+    public boolean add(T elem) {
+
+        // Check if the value already exists in this
+        // binary tree, if it does ignore adding it
+        if (contains(elem)) {
+            return false;
+
+            // Otherwise add this element to the binary tree
+        } else {
+            root = add(root, elem);
+            nodeCount++;
+            return true;
+        }
+    }
+
+    // Private method to recursively add a value in the binary tree
+    private Node add(Node node, T elem) {
+
+        // Base case: found a leaf node
+        if (node == null) {
+            node = new Node(null, null, elem);
+
+        } else {
+            // Pick a subtree to insert element
+            if (elem.compareTo(node.data) < 0) {
+                node.left = add(node.left, elem);
+            } else {
+                node.right = add(node.right, elem);
+            }
+        }
+
+        return node;
+    }
+
+    // Remove a value from this binary tree if it exists, O(n)
+    public boolean remove(T elem) {
+
+        // Make sure the node we want to remove
+        // actually exists before we remove it
+        if (contains(elem)) {
+            root = remove(root, elem);
+            nodeCount--;
+            return true;
+        }
+        return false;
+    }
+
+    private Node remove(Node node, T elem) {
+
+        if (node == null) return null;
+
+        int cmp = elem.compareTo(node.data);
+
+        // Dig into left subtree, the value we're looking
+        // for is smaller than the current value
+        if (cmp < 0) {
+            node.left = remove(node.left, elem);
+
+            // Dig into right subtree, the value we're looking
+            // for is greater than the current value
+        } else if (cmp > 0) {
+            node.right = remove(node.right, elem);
+
+            // Found the node we wish to remove
+        } else {
+
+            // This is the case with only a right subtree or
+            // no subtree at all. In this situation just
+            // swap the node we wish to remove with its right child.
+            if (node.left == null) {
+
+                Node rightChild = node.right;
+
+                node.data = null;
+                node = null;
+
+                return rightChild;
+
+                // This is the case with only a left subtree or
+                // no subtree at all. In this situation just
+                // swap the node we wish to remove with its left child.
+            } else if (node.right == null) {
+
+                Node leftChild = node.left;
+
+                node.data = null;
+                node = null;
+
+                return leftChild;
+
+                // When removing a node from a binary tree with two links the
+                // successor of the node being removed can either be the largest
+                // value in the left subtree or the smallest value in the right
+                // subtree. In this implementation I have decided to find the
+                // smallest value in the right subtree which can be found by
+                // traversing as far left as possible in the right subtree.
+            } else {
+
+                // Find the leftmost node in the right subtree
+                Node tmp = findMin(node.right);
+
+                // Swap the data
+                node.data = tmp.data;
+
+                // Go into the right subtree and remove the leftmost node we
+                // found and swapped data with. This prevents us from having
+                // two nodes in our tree with the same value.
+                node.right = remove(node.right, tmp.data);
+
+                // If instead we wanted to find the largest node in the left
+                // subtree as opposed to smallest node in the right subtree
+                // here is what we would do:
+                // Node tmp = findMax(node.left);
+                // node.data = tmp.data;
+                // node.left = remove(node.left, tmp.data);
+
+            }
+        }
+
+        return node;
+    }
+
+    // Helper method to find the leftmost node (which has the smallest value)
+    private Node findMin(Node node) {
+        while (node.left != null) node = node.left;
+        return node;
+    }
+
+    // Helper method to find the rightmost node (which has the largest value)
+    private Node findMax(Node node) {
+        while (node.right != null) node = node.right;
+        return node;
+    }
+
+    // returns true is the element exists in the tree
+    public boolean contains(T elem) {
+        return contains(root, elem);
+    }
+
+    // private recursive method to find an element in the tree
+    private boolean contains(Node node, T elem) {
+
+        // Base case: reached bottom, value not found
+        if (node == null) return false;
+
+        int cmp = elem.compareTo(node.data);
+
+        // Dig into the left subtree because the value we're
+        // looking for is smaller than the current value
+        if (cmp < 0) return contains(node.left, elem);
+
+            // Dig into the right subtree because the value we're
+            // looking for is greater than the current value
+        else if (cmp > 0) return contains(node.right, elem);
+
+            // We found the value we were looking for
+        else return true;
+    }
+
+    // Computes the height of the tree, O(n)
+    public int height() {
+        return height(root);
+    }
+
+    // Recursive helper method to compute the height of the tree
+    private int height(Node node) {
+        if (node == null) return 0;
+        return Math.max(height(node.left), height(node.right)) + 1;
+    }
+
+    // This method returns an iterator for a given TreeTraversalOrder.
+    // The ways in which you can traverse the tree are in four different ways:
+    // preorder, inorder, postorder and levelorder.
+    public Iterator<T> traverse(TreeTraversalOrder order) {
+        return switch (order) {
+            case PRE_ORDER -> preOrderTraversal();
+            case IN_ORDER -> inOrderTraversal();
+            case POST_ORDER -> postOrderTraversal();
+            case LEVEL_ORDER -> levelOrderTraversal();
+            default -> null;
+        };
+    }
+
+    // Returns as iterator to traverse the tree in pre order
+    private Iterator<T> preOrderTraversal() {
+
+        final int expectedNodeCount = nodeCount;
+        final java.util.Stack<Node> stack = new java.util.Stack<>();
+        stack.push(root);
+
+        return new java.util.Iterator<T>() {
+            @Override
+            public boolean hasNext() {
+                if (expectedNodeCount != nodeCount) throw new java.util.ConcurrentModificationException();
+                return root != null && !stack.isEmpty();
+            }
+
+            @Override
+            public T next() {
+                if (expectedNodeCount != nodeCount) throw new java.util.ConcurrentModificationException();
+                Node node = stack.pop();
+                if (node.right != null) stack.push(node.right);
+                if (node.left != null) stack.push(node.left);
+                return node.data;
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        };
+    }
+
+    // Returns as iterator to traverse the tree in order
+    private java.util.Iterator<T> inOrderTraversal() {
+
+        final int expectedNodeCount = nodeCount;
+        final java.util.Stack<Node> stack = new java.util.Stack<>();
+        stack.push(root);
+
+        return new java.util.Iterator<T>() {
+            Node trav = root;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedNodeCount != nodeCount) throw new java.util.ConcurrentModificationException();
+                return root != null && !stack.isEmpty();
+            }
+
+            @Override
+            public T next() {
+
+                if (expectedNodeCount != nodeCount) throw new java.util.ConcurrentModificationException();
+
+                // Dig left
+                while (trav != null && trav.left != null) {
+                    stack.push(trav.left);
+                    trav = trav.left;
+                }
+
+                Node node = stack.pop();
+
+                // Try moving down right once
+                if (node.right != null) {
+                    stack.push(node.right);
+                    trav = node.right;
+                }
+
+                return node.data;
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        };
+    }
+
+    // Returns as iterator to traverse the tree in post order
+    private java.util.Iterator<T> postOrderTraversal() {
+        final int expectedNodeCount = nodeCount;
+        final java.util.Stack<Node> stack1 = new java.util.Stack<>();
+        final java.util.Stack<Node> stack2 = new java.util.Stack<>();
+        stack1.push(root);
+        while (!stack1.isEmpty()) {
+            Node node = stack1.pop();
+            if (node != null) {
+                stack2.push(node);
+                if (node.left != null) stack1.push(node.left);
+                if (node.right != null) stack1.push(node.right);
+            }
+        }
+        return new java.util.Iterator<T>() {
+            @Override
+            public boolean hasNext() {
+                if (expectedNodeCount != nodeCount) throw new java.util.ConcurrentModificationException();
+                return root != null && !stack2.isEmpty();
+            }
+
+            @Override
+            public T next() {
+                if (expectedNodeCount != nodeCount) throw new java.util.ConcurrentModificationException();
+                return stack2.pop().data;
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        };
+    }
+
+    // Returns as iterator to traverse the tree in level order
+    private java.util.Iterator<T> levelOrderTraversal() {
+
+        final int expectedNodeCount = nodeCount;
+        final java.util.Queue<Node> queue = new java.util.LinkedList<>();
+        queue.offer(root);
+
+        return new java.util.Iterator<T>() {
+            @Override
+            public boolean hasNext() {
+                if (expectedNodeCount != nodeCount) throw new java.util.ConcurrentModificationException();
+                return root != null && !queue.isEmpty();
+            }
+
+            @Override
+            public T next() {
+                if (expectedNodeCount != nodeCount) throw new java.util.ConcurrentModificationException();
+                Node node = queue.poll();
+                assert node != null;
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+                return node.data;
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        };
     }
 }
